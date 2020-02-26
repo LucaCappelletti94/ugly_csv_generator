ea5f401fea41d4516f19b37e1055f551f9339754
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-26 22:18:16 +0100

    Extended testing

--- tests/test_uglify.py
+++ tests/test_uglify.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 def test_uglify():
-    for _ in range(3):
+    for i in range(3):
         csv = random_csv()
-        ugly1 = uglify(csv)
-        ugly2 = uglify(csv)
+        ugly1 = uglify(csv, seed=i)
+        ugly2 = uglify(csv, seed=i)
 
         pd.testing.assert_frame_equal(ugly1, ugly2)
