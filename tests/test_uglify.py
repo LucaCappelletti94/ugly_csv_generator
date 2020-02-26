a2a17b58b5d45673a64fa6e6b608f5c40908f81b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-26 22:23:25 +0100

    Published new version

--- tests/test_uglify.py
+++ tests/test_uglify.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def test_uglify():
-    for i in range(3):
+    for i in range(10):
         csv = random_csv()
         ugly1 = uglify(csv, seed=i)
         ugly2 = uglify(csv, seed=i)
